4af90577139c0991aaafcdf3a87832fef46cbd40
ik04 <ishaankhurana123@gmail.com>
2023-06-21 03:30:49 +0530

    revert changes

--- App/firewatch/pages/wildfire.js
+++ App/firewatch/pages/wildfire.js
@@ -11,7 +11,7 @@ const wildfire = () => {
   const [show, setShow] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const apiKey = process.env.API_KEY;
+  const apiKey = "HGgzNs8K2j8VrmMgNshhbbf4w1QYkKTUng0oBA5v";
   // const [thumbnailUrl, setThumbnailUrl] = useState();
   const url = "https://firewatch-5kkw.onrender.com/api/";
   const options = [
