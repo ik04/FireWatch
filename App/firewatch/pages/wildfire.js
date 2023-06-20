6e6b166baad6f3cd08ad64326610a5d6086d5afb
ik04 <ishaankhurana123@gmail.com>
2023-06-21 02:22:27 +0530

    update api endpoints

--- App/firewatch/pages/wildfire.js
+++ App/firewatch/pages/wildfire.js
@@ -13,7 +13,7 @@ const wildfire = () => {
 
   const apiKey = "HGgzNs8K2j8VrmMgNshhbbf4w1QYkKTUng0oBA5v";
   // const [thumbnailUrl, setThumbnailUrl] = useState();
-  const url = "http://127.0.0.1:8000/api/";
+  const url = "https://firewatch-5kkw.onrender.com/api/";
   const options = [
     { lat: -3.23, lon: -62.23, date: "2019-03-03", name: "Amazon Forest" },
     { lat: 61.01, lon: 28.75, date: "2020-10-29", name: "Tiaga Forest" },
@@ -99,117 +99,117 @@ const wildfire = () => {
   };
   return (
     <div className="h-screen xl:overflow-x-hidden xl:overflow-y-hidden">
-    <Navbar/>
-    <div className="h-full flex flex-col bg-gradient-to-r from-orange-500 to-red-500">
-      <button
-        className="text-white hover:scale-105 self-center my-8 mx-5 px-5 py-2 bg-white-500 bg-opacity-50 rounded-full xl:text-xl border-2"
-        onClick={handleSwitch}
+      <Navbar />
+      <div className="h-full flex flex-col bg-gradient-to-r from-orange-500 to-red-500">
+        <button
+          className="text-white hover:scale-105 self-center my-8 mx-5 px-5 py-2 bg-white-500 bg-opacity-50 rounded-full xl:text-xl border-2"
+          onClick={handleSwitch}
         >
-        SWITCH
-      </button>
-      <div className="flex justify-center">
-        {show ? (
-          <>
-            <form onSubmit={callNasaApi}>
-              <select
-                className="text-2xl p-2 rounded-xl"
-                onChange={(e) => setDrop(options[e.target.value])}
+          SWITCH
+        </button>
+        <div className="flex justify-center">
+          {show ? (
+            <>
+              <form onSubmit={callNasaApi}>
+                <select
+                  className="text-2xl p-2 rounded-xl"
+                  onChange={(e) => setDrop(options[e.target.value])}
                 >
-                {options.map((option, index) => (
-                  <option key={index} value={index}>
-                    {`${option.name}`}
-                  </option>
-                ))}
-              </select>
+                  {options.map((option, index) => (
+                    <option key={index} value={index}>
+                      {`${option.name}`}
+                    </option>
+                  ))}
+                </select>
 
-              <button
-                className="text-white hover:scale-105 self-center my-8 mx-5 px-5 py-2 bg-white-500 bg-opacity-50 rounded-full xl:text-xl border-2"
-                type="submit"
-                >
-                Submit
-              </button>
-            </form>
-          </>
-        ) : (
-          <div className="flex flex-col">
-            <div className="flex flex-col">
-              <form
-                className="flex items-center justify-center"
-                onSubmit={callNasaApi}
-                >
-                <label
-                  className="text-white text-2xl font-semibold mx-2"
-                  htmlFor="lat"
-                  >
-                  latitude
-                </label>
-                <input
-                  className="rounded-xl p-2"
-                  step=".001"
-                  type="number"
-                  name="lat"
-                  onChange={(e) => setLatitude(e.target.value)}
-                  />
-                <label
-                  className="text-2xl text-white mx-2 font-semibold"
-                  htmlFor="lon"
-                  >
-                  longitude
-                </label>
-                <input
-                  className="rounded-xl p-2"
-                  step=".001"
-                  type="number"
-                  name="lon"
-                  onChange={(e) => setLongitude(e.target.value)}
-                  />
-                <input
-                  className="font-semibold mx-6 rounded-xl p-2"
-                  type="date"
-                  name="date"
-                  onChange={(e) => setDate(e.target.value)}
-                  />
                 <button
+                  className="text-white hover:scale-105 self-center my-8 mx-5 px-5 py-2 bg-white-500 bg-opacity-50 rounded-full xl:text-xl border-2"
                   type="submit"
-                  className="text-2xl px-3 py-2 text-white rounded-3xl bg-white-400 border-2"
-                  >
+                >
                   Submit
                 </button>
-
-                {/* violated DRY :[ */}
               </form>
+            </>
+          ) : (
+            <div className="flex flex-col">
+              <div className="flex flex-col">
+                <form
+                  className="flex items-center justify-center"
+                  onSubmit={callNasaApi}
+                >
+                  <label
+                    className="text-white text-2xl font-semibold mx-2"
+                    htmlFor="lat"
+                  >
+                    latitude
+                  </label>
+                  <input
+                    className="rounded-xl p-2"
+                    step=".001"
+                    type="number"
+                    name="lat"
+                    onChange={(e) => setLatitude(e.target.value)}
+                  />
+                  <label
+                    className="text-2xl text-white mx-2 font-semibold"
+                    htmlFor="lon"
+                  >
+                    longitude
+                  </label>
+                  <input
+                    className="rounded-xl p-2"
+                    step=".001"
+                    type="number"
+                    name="lon"
+                    onChange={(e) => setLongitude(e.target.value)}
+                  />
+                  <input
+                    className="font-semibold mx-6 rounded-xl p-2"
+                    type="date"
+                    name="date"
+                    onChange={(e) => setDate(e.target.value)}
+                  />
+                  <button
+                    type="submit"
+                    className="text-2xl px-3 py-2 text-white rounded-3xl bg-white-400 border-2"
+                  >
+                    Submit
+                  </button>
+
+                  {/* violated DRY :[ */}
+                </form>
+              </div>
             </div>
+          )}
+        </div>
+        {result ? (
+          <div className="flex justify-center items-center flex-col mt-5">
+            <h1 className="text-5xl text-white font-semibold">{result}</h1>
+            <img
+              className="w-[500px] mt-5"
+              src={`https://firewatch-5kkw.onrender.com${displayImage}`}
+              alt=""
+            />
           </div>
+        ) : (
+          loading && (
+            <div className="flex justify-center items-center">
+              <div class="lds-roller w-36 mx-5">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+              <h1 className="self-center text-white text-3xl">loading...</h1>
+            </div>
+          )
         )}
       </div>
-      {result ? (
-        <div className="flex justify-center items-center flex-col mt-5">
-          <h1 className="text-5xl text-white font-semibold">{result}</h1>
-          <img
-            className="w-[500px] mt-5"
-            src={`http://127.0.0.1:8000${displayImage}`}
-            alt=""
-            />
-        </div>
-      ) : (
-        loading && (
-          <div className="flex justify-center items-center">
-            <div class="lds-roller w-36 mx-5">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-            <h1 className="self-center text-white text-3xl">loading...</h1>
-          </div>
-        )
-        )}
     </div>
-        </div>
   );
 };
 
