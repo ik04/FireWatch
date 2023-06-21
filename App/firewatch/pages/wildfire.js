d814fd2f762009731d436b65d213dd62c6b70837
ik04 <ishaankhurana123@gmail.com>
2023-06-21 05:50:31 +0530

    rectify previous commit

--- App/firewatch/pages/wildfire.js
+++ App/firewatch/pages/wildfire.js
@@ -134,7 +134,7 @@ const wildfire = () => {
                   ))}
                 </select>
 
-                {isSubmit ? (
+                {!isSubmit ? (
                   <button
                     className="text-white hover:scale-105 self-center my-8 mx-5 px-5 py-2 bg-white-500 bg-opacity-50 rounded-full xl:text-xl border-2"
                     type="submit"
